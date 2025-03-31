4ef442cf4d31af201b7bc637fb163cd9984846f3
Osbaldo R <osbaldor025@gmail.com>
2025-03-30 22:31:09 -0600

    Nuevo componente View (styles.box) envolviendo al texto.

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>VAYANSE A LA PORRA</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>Hola</Text>
+      </View>
     </View>
   );
 }
@@ -14,10 +16,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  box: {
     backgroundColor: '#fff',
+    padding: 30,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    elevation: 3
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#e91e63'  // Rosa
   },
 });
