2c83907ee4f7fb5af260b7aba078c605e4590bf8
Osbaldo R <osbaldor025@gmail.com>
2025-03-30 22:36:02 -0600

    Uso de Animated y hooks (useRef, useEffect).

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Animated } from 'react-native';
 
 export default function App() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: true
+      }
+    ).start();
+  }, [fadeAnim]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>VAYANSE A LA PORRA</Text>
+      <Animated.Text style={[styles.text, { opacity: fadeAnim }]}>
+        Hola
+      </Animated.Text>
     </View>
   );
 }
@@ -19,5 +34,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#ff9800'  // Naranja
   },
 });
