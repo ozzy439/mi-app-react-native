2e6942978d98f8bbdd5bec3b394d022b1c9257b7
Osbaldo R <osbaldor025@gmail.com>
2025-03-30 21:45:04 -0600

    Agregué vayanse a la porra

---
+++ Screens/sketch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './Screens/LoginScreen';
+import RegistroScreen from './Screens/RegistroScreen';
+import HomeScreen from './Screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registro" component={RegistroScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
